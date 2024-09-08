33412fe7b688143270f0c94b010341fbaaafaf4f
AbdurRahim80 <mdrahim.nil2005@gmail.com>
2024-09-09 00:20:51 +0600

    data get from databas mongoDB

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.qgxn6jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qgxn6jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = "mongodb+srv://dreamHostel:HBqd027jA3zBYwjH@cluster0.qgxn6jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,16 +28,53 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
-    // rooms collection 
-    const roomsCollection = client.db('dreamHostel').collection('hostel')
+    const roomsCollection = client.db('dreamHostel').collection('hostel');
+    const bookingCollection = client.db('bookingCollection').collection('bookings');
 
     app.get('/rooms', async(req, res)=>{
         const cursor = roomsCollection.find();
-        const result = await cursor.toArray()
-        res.send(result)
-        
+        const result = await cursor.toArray();
+        res.send(result);
     })
+
+    app.get('/rooms/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await roomsCollection.findOne(query);
+        res.send(result);
+    })
+
+
+    // bookings start
+  //   app.post('/bookings', async(req, res)=>{
+  //     const bookingList = req.body;
+  //     // console.log(bookingList);
+  //     const result = await bookingCollection.insertOne(bookingList);
+  //     res.send(result);
+  // })
+
+  // app.get('/bookings', async(req, res)=>{
+  //   const cursor = bookingCollection.find();
+  //   const result = await cursor.toArray();
+  //   // console.log(result);
+  //   res.send(result);
+  // })
+
+  // app.get('/bookings/:email', async(req, res)=>{
+  //   console.log(req.params.email);
+  //   const result = await bookingCollection.find({email: req.params.email}).toArray();
+  //   console.log(result);
+  //   res.send(result);
+  // })
+
+  // app.delete('/bookings/:id', async(req, res)=>{
+  //   const id = req.params.id;
+  //   const query = {_id: new ObjectId(id)};
+  //   const result = await bookingCollection.deleteOne(query);
+  //   res.send(result);
+  // })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -48,11 +86,10 @@ async function run() {
 run().catch(console.dir);
 
 
-
 app.get('/', (req, res) => {
-    res.send("server is running")
+    res.send("server is  running")
 })
 
 app.listen(port, () => {
-    console.log(`server is running port${port}`);
+    console.log(`server is rs running port${port}`);
 })
